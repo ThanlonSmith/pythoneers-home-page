10576a9b3a6629b1e4f5b49a343162efab8ad900
thanlonsmith <thanlon@163.com>
2020-10-30 16:17:40 +0800

    Update api url.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import Axios from "axios"
 import ElementUI from 'element-ui'
 
-Axios.defaults.baseURL = 'http://127.0.0.1:8000/v1/api/'
+// Axios.defaults.baseURL = 'http://127.0.0.1:8000/v1/'
+Axios.defaults.baseURL = 'http://api.pythoneers.cn/v1/'
+
 Vue.prototype.$http = Axios
 Vue.config.productionTip = false // 生产环境不需要控制台有提示
 Vue.use(ElementUI)
