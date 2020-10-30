ff42ca3508e56d1a8ee3c2b45f925a77dea18f7e
thanlonsmith <thanlon@163.com>
2020-10-30 20:01:24 +0800

    Released v1.0.0 firstly.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Axios from "axios"
 import ElementUI from 'element-ui'
 
 // Axios.defaults.baseURL = 'http://127.0.0.1:8000/v1/'
-Axios.defaults.baseURL = 'http://api.pythoneers.cn/v1/'
+Axios.defaults.baseURL = 'https://api.pythoneers.cn/v1/'
 
 Vue.prototype.$http = Axios
 Vue.config.productionTip = false // 生产环境不需要控制台有提示
